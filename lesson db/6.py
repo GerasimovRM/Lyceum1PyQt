e141aa14b2a58663858d1c46bd004703bae18308
Roman M. Gerasimov <48310403+GerasimovRM@users.noreply.github.com>
2021-11-22 20:33:02 +0500

    Update 6.py

--- lesson db/6.py
+++ lesson db/6.py
@@ -5,7 +5,3 @@ cursor = connection.cursor()
 
 cursor.execute("UPDATE films SET title='Нормальное название фильма' WHERE id=18004")
 connection.commit()
-
-import PyQt5.QtCore
-
-print(PyQt5.QtCore.black)
